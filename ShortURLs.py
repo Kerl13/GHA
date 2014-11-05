ac505d50eb1249bf071402ce37e58367f44b2dd1
u4xmp <nicojpub@gmail.com>
2014-11-05 16:33:12 +0100

    Dirty correction of url shortener problem

--- ShortURLs.py
+++ ShortURLs.py
@@ -12,20 +12,15 @@ class ShortURLs:
     It's not fully working atm.
     '''
 
-    def __init__(self, base_url, save_file = None):
+    def __init__(self, base_url, save_file):
         self.base_url = base_url
         self.save_file = save_file
-        if save_file:
-            try:
-                self.url_list = loads( open(save_file, 'r').read() )['content']
-            except:
-                self.url_list = []
-        else:
-            self.url_list = []
-        
-    def _save(self):
-        if self.save_file:
-            open(self.save_file, 'w+').write( dumps( {'content':self.url_list}, indent=4 ) )
+
+    def get_list(self):
+        return load( open(self.save_file, 'r').read() )['content']
+
+    def set_list(self, l):
+        open(self.save_file, 'w+').write( dumps( {'content':l}, indent=4 ) )
 
     def string_to_int(self, string):
         return int(string) # to be changed
@@ -34,28 +29,19 @@ class ShortURLs:
         return str(i) # to be changed
 
     def url_to_short(self, url):
-        print 'url_to_short before', self.url_list
-        if url in self.url_list:
-            i = self.url_list.index(url)
+        l = self.get_list()
+        if url in l:
+            i = l.index(url)
         else:
-            i = len(self.url_list)
-            self.url_list += [url]
-            self._save()
-        print 'url_to_short modif', url, i
-        print 'url_to_short after', self.url_list        
+            i = len(l)
+            l.append( url )
+            self.set_list(l)
         return self.base_url+self.int_to_string(i)
 
     def short_to_url(self, short, with_base_url=True):
-        try:
-            print 'short_to_url before', self.url_list
-            if with_base_url:
-                i = self.string_to_int( short[len(self.base_url):] )
-            else:
-                i = self.string_to_int( short )
-            url = self.url_list[i]
-            print 'short_to_url modif', i, url
-            print 'short_to_url after', self.url_list
-            return url
-        except:
-            return ''
-
+        if with_base_url:
+            i = self.string_to_int( short[len(self.base_url):] )
+        else:
+            i = self.string_to_int( short )
+        url = self.get_list()[i]
+        return url
