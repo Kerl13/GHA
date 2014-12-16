97fb7a45017e6bf144fdc2c7b7ae9d1423d5dc78
Nicolas Jeannerod <nicojpub@gmail.com>
2014-12-16 01:37:15 +0100

    Last change for URL Shortener. Fix #3

--- GHA.py
+++ GHA.py
@@ -40,14 +40,6 @@ parser.add_argument('-lp', '--listen-port',
                     type = int,
                     help = 'the port where GHA will be listening')
 
-parser.add_argument('-wh', '--web-host',
-                    type = str,
-                    help = 'the address seen from outside (if you\'re behind a proxy…)')
-
-parser.add_argument('-wp', '--web-port',
-                    type = str,
-                    help = 'the port seen from outside')
-
 parser.add_argument('-ih', '--irc-host',
                     type = str,
                     help = 'the irc server\'s address')
@@ -74,14 +66,6 @@ parser.add_argument('-ia', '--import-arguments',
                     type = str, # à rendre plus précis
                     help = 'import arguments from the given file')
 
-parser.add_argument('-ll', '--log-level',
-                    type = str, # à rendre plus précis
-                    help = 'the log level wanted (DEBUG, INFO, WARNING, ERROR)')
-
-parser.add_argument('-lf', '--log-file',
-                    type = str, # à rendre plus précis
-                    help = 'the log file')
-
 ARGS = parser.parse_args()
 
 if ARGS.import_arguments:
@@ -100,13 +84,6 @@ if ARGS.import_arguments:
 #                V.prnt (line, V.ERROR)
         exit(1)
 
-#if not ARGS.verbose_level:
-#    V.level = V.WARNING
-#    V.prnt('No verbose level given. Using WARNING.', V.WARNING)
-#    ARGS.verbose_level = 'WARNING'
-#else:
-#    V.set_level(ARGS.verbose_level)
-
 if not ARGS.listen_host:
 #    V.prnt('No listen host given. Using 0.0.0.0.', V.WARNING)
     ARGS.listen_host = '0.0.0.0'
@@ -114,14 +91,6 @@ if not ARGS.listen_host:
 if not ARGS.listen_port:
 #    V.prnt('No listen port given. Using 80.', V.WARNING)
     ARGS.listen_port = 80
-
-if not ARGS.web_host:
-#    V.prnt('No web host given. Using listen host.', V.WARNING)
-    ARGS.web_host = ARGS.listen_host
-
-if not ARGS.web_port:
-#    V.prnt('No web port given. Using listen port.', V.WARNING)
-    ARGS.web_port = ARGS.listen_port
 
 if not ARGS.irc_host:
 #    V.prnt('No IRC host given. Using localhost.', V.WARNING)
