a5e543b82b1ed1342f11361a3fb2106fb4d2792a
Nicolas Jeannerod <niols@niols.net>
2015-06-15 16:40:25 -0400

    Change URL Shortener from niols' one to is.gd

--- URLShortener.py
+++ URLShortener.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from urllib import quote_plus
 from urllib2 import urlopen
 
 def short (url):
-    short_url = urlopen ('http://niols.net/u/?api_url=' + url)
+    short_url = urlopen ('http://is.gd/create.php?format=simple&url=' + quote_plus(url))
     return short_url.readline ()
 
